3840cffa5631031842b3609707f1ae0c6f576b37
noornabi07 <noornabiprogram07@gmail.com>
2023-05-06 11:18:56 +0600

    category path location setup

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import NewsCard from '../News/NewsCard/NewsCard';
+import useTitle from '../../Hooks/useTitle';
 
 const Category = () => {
     const {id} = useParams();
     const categoryNews = useLoaderData();
+    useTitle('Category')
     return (
         <div>
             {id && <h2>This is category news: {categoryNews.length}</h2>}
